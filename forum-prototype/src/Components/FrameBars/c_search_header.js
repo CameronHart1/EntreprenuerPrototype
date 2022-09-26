2d8d965b7baaa4d2a91d56f9ec9cf5e4df2cc0b3
CameronHart1 <82789385+CameronHart1@users.noreply.github.com>
2022-09-27 00:27:32 +1000

    Search Header elements added
    No functionality yet, but the write elements are in place and styled

--- forum-prototype/src/Components/FrameBars/c_search_header.js
+++ forum-prototype/src/Components/FrameBars/c_search_header.js
@@ -1,11 +1,40 @@
-import '../../CSS/c_search_header.css'
+import "../../CSS/c_search_header.css";
 
 export const SearchHeader = () => {
+  return (
+    <div className="SearchWrapper">
+      <LeftWidgets />
+      <SearchBar />
+      <RightWidgets />
+    </div>
+  );
+};
+const LeftWidgets = () => {
+  return (
+    <div id="LW_Wrapper">
+      <img src={`${process.env.PUBLIC_URL}\\icons\\Search\\Discovery.svg`} />
+      <p>Browse</p>
+      <img src={`${process.env.PUBLIC_URL}\\icons\\small-right.svg`} />
+    </div>
+  );
+};
 
-    return(
-        <div className="SearchWrapper">
-            <h1>something</h1>
-        </div>
-    )
-}
+const SearchBar = () => {
+  return (
+    <div id="SearchBar">
+        <img  src={`${process.env.PUBLIC_URL}\\icons\\Search\\Search.svg`}/>
+        <input id="SearchIn" type="text" placeholder="Search Everything"/>
+    </div>
+  );
+};
 
+const RightWidgets = () => {
+  return (
+    <div id="RW_Wrapper">
+      <img src={`${process.env.PUBLIC_URL}\\icons\\Search\\Plus.svg`} />
+      <img src={`${process.env.PUBLIC_URL}\\icons\\Chat.svg`} />
+      <img src={`${process.env.PUBLIC_URL}\\icons\\Search\\Notification.svg`} />
+      <div id="ProfilePic" />
+    </div>
+  );
+};
