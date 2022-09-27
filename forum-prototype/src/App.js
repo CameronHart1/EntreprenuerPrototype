ff61875299eb795391aa961111a4beff11efa032
CameronHart1 <82789385+CameronHart1@users.noreply.github.com>
2022-09-27 12:08:05 +1000

    PostPage
    Visuals / elements for the Post page completed needs functionality
    
    Also copied some of the parts of the Sign in's blue button to a seperate css, so we can easily use them in other pages

--- forum-prototype/src/App.js
+++ forum-prototype/src/App.js
@@ -1,57 +1,55 @@
-
 import React, { useEffect } from "react";
 import "./CSS/App.css";
 
 // Routing stuff
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Frame from "./pages/Frame";
-import {NewsFeed} from "./pages/NewsFeed";
+import { NewsFeed } from "./pages/NewsFeed";
 import NoPage from "./pages/NoPage";
 import { SignIn } from "./pages/SignIn";
+import PostPage from "./pages/Post";
 // import MakePost from "./pages/MakePost";
 // import LoginPage from "./pages/Login";
 
-
 const App = () => {
-    // const [, setData] = React.useState(null);
-    // const [posts, setPosts] = React.useState([{}]);
-    // const [accounts, setAccounts] = React.useState([{}]);
-  
-    // useEffect(() => {
-    //   // getting JSONs
-    //   fetch("/Posts.json")
-    //     .then((res) => res.json())
-    //     .then((Jdata) => {
-    //       setPosts(Jdata);
-    //       // console.log("Parsed Articles");
-    //     });
-    //   fetch("/Accounts.json")
-    //     .then((res) => res.json())
-    //     .then((Jdata) => {
-    //       setAccounts(Jdata);
-    //       // console.log("Parsed Tutorials");
-    //     });
-    // }, []);
+  // const [, setData] = React.useState(null);
+  // const [posts, setPosts] = React.useState([{}]);
+  // const [accounts, setAccounts] = React.useState([{}]);
+
+  // useEffect(() => {
+  //   // getting JSONs
+  //   fetch("/Posts.json")
+  //     .then((res) => res.json())
+  //     .then((Jdata) => {
+  //       setPosts(Jdata);
+  //       // console.log("Parsed Articles");
+  //     });
+  //   fetch("/Accounts.json")
+  //     .then((res) => res.json())
+  //     .then((Jdata) => {
+  //       setAccounts(Jdata);
+  //       // console.log("Parsed Tutorials");
+  //     });
+  // }, []);
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Frame/>}>
-            <Route
-              index
-             element={<Navigate to="/newsfeed"/>}
-            />
-            <Route path="newsfeed" element={<NewsFeed/>}/>
+          <Route path="/" element={<Frame />}>
+            <Route index element={<Navigate to="/newsfeed" />} />
+            <Route path="newsfeed" element={<NewsFeed />} />
+            <Route path="post" element={<PostPage />} />
             {/* <Route path="/makepost" element={<MakePost />} /> */}
             {/* <Route path="/login/:SignType" element={<LoginPage />} /> */}
             {/* <Route path="blogs" element={<Blogs />} />
             <Route path="contact" element={<Contact />} /> */}
             <Route path="*" element={<NoPage />} />
           </Route>
-          <Route path="/account/:signtype" element={<SignIn/>}/>
+          {/* <Route path="*" element={<NoPage />} /> */}
+          <Route path="/account/:signtype" element={<SignIn />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
-}
+};
 export default App;
---
+++ forum-prototype/src/pages/Post.js
@@ -0,0 +1,156 @@
+import { useState, useContext } from "react";
+import "../CSS/p_post.css";
+import "../CSS/CommonElements.css";
+
+const PostPage = () => {
+  const [content, setContent] = useState({
+    title: "",
+    content: "",
+    img: {},
+    tags: [],
+  });
+
+
+  // Changing Content data
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setContent((preValue) => {
+      if (name == "tags")
+        return {
+          ...preValue,
+          [name]: value.split(/(?:,| |#)+/).filter((s) => s),
+        };
+      return {
+        ...preValue,
+        [name]: value,
+      };
+    });
+  };
+
+  return (
+    <div id="PostWrapper">
+      <h1 id="PostTitle">Post</h1>
+      {/* Title */}
+      <TitleComp handleTextChange={handleChange} />
+
+      {/* Tags */}
+      <TagComp handleTextChange={handleChange} />
+
+      {/* Content box */}
+      <LargeTextBox
+        handleTextChange={handleChange}
+        title="Content"
+        placeHolder=""
+        wordLimit="700"
+        Rows="40"
+        name="content"
+        Columns="18"
+      />
+
+      {/* IMG */}
+      <ImageComp handleImageChange={handleChange} default={content.image} />
+      {/* ------------------------------
+       Submit Button*/}
+      <PostButton active={(content.title != "" && content.content != "" && content.tags.length > 0)}/>
+    </div>
+  );
+};
+
+// Components
+const TitleComp = (props) => {
+  return (
+    <div className="IdDiv">
+      <h1 className="EntryTitle">Title</h1>
+      <input
+        type="text"
+        name="title"
+        className="RoundedDiv ThinBox"
+        placeholder=""
+        onChange={props.handleTextChange}
+      />
+    </div>
+  );
+};
+// -----------------------------------------------------------------------------------
+const ImageComp = (props) => {
+  const [fileName, setFileName] = useState("");
+
+  const uploadFile = (e) => {
+    props.handleImageChange(e);
+    setFileName(e.target.files[0].name);
+  };
+
+  return (
+    <div>
+      <h1 className="EntryTitle">Insert images</h1>
+      <div className="inputfile-box RoundedDiv ThinBox">
+        <input
+          type="file"
+          id="ImageInput"
+          defaultValue={props.default}
+          class="inputfile"
+          accept="image/*"
+          onChange={uploadFile}
+        />
+        <label for="ImageInput">
+          <span id="file-name" class="file-box">
+            {fileName}
+          </span>
+          <span class="file-button">
+            <i aria-hidden="true"></i>
+            Open browser
+          </span>
+        </label>
+      </div>
+    </div>
+  );
+};
+// -----------------------------------------------------------------------------------
+const LargeTextBox = (props) => {
+  const { WordLimit, Rows, Col, title, name, placeHolder } = props;
+  const startText = props.text ? props.text : "";
+  return (
+    <div>
+      <h1 className="EntryTitle">{title}</h1>
+      <textarea
+        type="text"
+        id="TextInput"
+        className="RoundedDiv"
+        name={name}
+        maxLength={WordLimit}
+        rows={Rows}
+        cols={Col}
+        placeholder={placeHolder}
+        onChange={props.handleTextChange}
+        defaultValue={startText}
+      />
+    </div>
+  );
+};
+// -----------------------------------------------------------------------------------
+const TagComp = (props) => {
+  return (
+    <div className="IdDiv">
+      <h1 className="EntryTitle">Tags</h1>
+      <input
+        type="text"
+        name="tags"
+        className="RoundedDiv ThinBox"
+        placeholder='At least 3 tags, seperate with:" ",#'
+        // using regex to split hashtags and filtertring out any empty values
+        onChange={props.handleTextChange}
+      />
+    </div>
+  );
+};
+
+// -------------------------------------------------------------------------------------
+const PostButton = (props) => {
+  return (
+    <div>
+      <button id="PostButton" className="BlueButton Shadow" disabled={!props.active}>Post</button>
+    </div>
+  );
+};
+
+export default PostPage;
